fc9f25c121d532288dcef4d5c4cec2c1e99e5a45
JKyEun <j56237@naver.com>
2023-10-01 14:25:47 +0900

    chore: next.config.js 프록시 우회 설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: `http://localhost:3000/:path*`
+            }
+        ];
+    }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
