d57ae2d08e68ced44e54dc74d8553766b964ad2f
JKyEun <j56237@naver.com>
2023-10-23 00:40:48 +0900

    feat: logo 클릭 이벤트(메인페이지로 이동) 구현

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,13 +1,24 @@
+'use client';
+
 import Image from 'next/image';
 import React from 'react';
 import '../style/navbar.scss';
+import { useRouter } from 'next/navigation';
 
 export default function Navbar() {
+    const router = useRouter();
+
+    const goStatPage = () => {
+        router.push('/');
+    };
+
     return (
         <div className='nav-bar'>
             <div className='nav-bar-container'>
                 <div className='left-side'>
-                    <div className='logo'>NBA-Simulator</div>
+                    <div onClick={goStatPage} className='logo'>
+                        NBA-Simulator
+                    </div>
                 </div>
                 <div className='right-side'>
                     <div className='item ranking'>
