45740b24adf5bc4c5356e80467493a33fd120752
JKyEun <j56237@naver.com>
2023-10-22 23:42:28 +0900

    design: navbar 버튼 퍼블리싱

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -9,6 +9,16 @@ export default function Navbar() {
                 <div className='left-side'>
                     <div className='logo'>NBA-Simulator</div>
                 </div>
+                <div className='right-side'>
+                    <div className='item ranking'>
+                        <Image src='/ranking.svg' alt='랭킹' width={20} height={20} />
+                        <span>RANKING</span>
+                    </div>
+                    <div className='item login'>
+                        <Image src='/login.svg' alt='랭킹' width={20} height={20} />
+                        <span>LOGIN</span>
+                    </div>
+                </div>
             </div>
         </div>
     );
