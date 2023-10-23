4b972ac430cdf7ec64b737d0edaa2572b53db44f
JKyEun <j56237@naver.com>
2023-10-24 01:42:59 +0900

    design: help page 퍼블리싱 중

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -21,13 +21,15 @@ export default function Navbar() {
                     </div>
                 </div>
                 <div className='right-side'>
+                    <div className='item credit'>Credit: 1</div>
+                    <div className='item help'>
+                        <Image src='/help.svg' alt='도움말' width={20} height={20} />
+                    </div>
                     <div className='item ranking'>
                         <Image src='/ranking.svg' alt='랭킹' width={20} height={20} />
-                        <span>RANKING</span>
                     </div>
                     <div className='item login'>
                         <Image src='/login.svg' alt='랭킹' width={20} height={20} />
-                        <span>LOGIN</span>
                     </div>
                 </div>
             </div>
---
+++ src/app/help/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../style/helpPage.scss';
+
+export default function page() {
+    return (
+        <div className='help-page'>
+            <div className='help-wrap'>
+                <h2>도움말</h2>
+                <div></div>
+            </div>
+        </div>
+    );
+}
