5469c768ffae2d24d2f10f065e7399a29c903303
JKyEun <j56237@naver.com>
2024-02-09 23:47:28 +0900

    chore: 카카오 리다이렉트 URI 설정

--- src/app/constants/kakaoLogin.ts
+++ src/app/constants/kakaoLogin.ts
@@ -1,4 +1,4 @@
 export const GRANT_TYPE = 'authorization_code';
 export const KAKAO_CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
-export const KAKAO_REDIRECT_URI = 'http://localhost:3000/oauth/callback/kakao';
+export const KAKAO_REDIRECT_URI = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
